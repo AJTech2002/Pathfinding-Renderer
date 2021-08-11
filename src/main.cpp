e8fddbd45c141e096712c95029e82a2631fa0b38
AJTech2002 <Ajay Venkat>
2021-08-11 16:54:24 +1000

    FEAT-1-Initialise

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+int main()
+{
+
+	return 0;
+}
