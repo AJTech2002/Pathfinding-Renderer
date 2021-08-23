2243fcd09e15f70185bcf31aa6ecbc78a53e1ca3
Ajay <bestbatchprogrammer101@gmail.com>
2021-08-23 13:55:10 +1000

    ref: readme updates

--- README.md
+++ README.md
@@ -14,6 +14,12 @@ The first method that will be explored is an addition to A\* known as JPS.
 
 # Next Steps
 
+- Creating Makefile for easier compilation
 - Creating a Grid Visualiser
 - Creating Shaders to Debug Paths
 - Selecting Grid Elements through Mouse Click
+
+# Compiling 
+
+- Make sure to change all the paths in render_loop.cpp to your local ones (they are absolute for testing purposes)
+
--- src/core/render_loop.cpp
+++ src/core/render_loop.cpp
@@ -49,7 +49,7 @@ void initScene ()
 
     scene.sceneShaders.push_back(shader);
 
-    Model capsuleModel("C:/Users/Ajay/Desktop/Pathfinding-Renderer/primitives/Cube.fbx", &scene.sceneShaders[0]);
+    Model capsuleModel("C:/Users/Ajay/Desktop/Pathfinding-Renderer/primitives/CapsuleSmooth.fbx", &scene.sceneShaders[0]);
 
     Light directionalLight;
     
@@ -58,7 +58,7 @@ void initScene ()
     scene.setupScene(directionalLight, camera);
 
     capsuleModel.meshes[0].tint = glm::vec3(107.0 / 256.0f, 190.0 / 256.0f, 242.0 / 256.0f);
-    capsuleModel.meshes[0].model = glm::scale(capsuleModel.meshes[0].model, glm::vec3(0.3f, 1.5f, 2.0f));
+    // capsuleModel.meshes[0].model = glm::scale(capsuleModel.meshes[0].model, glm::vec3(0.3f, 1.5f, 2.0f));
     scene.addSceneObject(capsuleModel);
     
     camera.projection = glm::perspective(glm::radians(60.0f), 800.0f / 800.0f, 0.1f, 1000.0f);
